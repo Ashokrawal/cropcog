a87c489ef79512abd1f8f43dfbcfcf7bba822309
AnanthAgriServices <71168912+AnanthAgriServices@users.noreply.github.com>
2023-08-16 09:28:54 +0530

    completed arragement

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ashok/Desktop/next/cropcog/src/app/layout\":[\"static/media/916d3686010a8de2-s.p.woff2\",\"static/media/7777133e901cd5ed-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
